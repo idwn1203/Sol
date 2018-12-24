5c3283148d3f192b42b595742486dcc23e7a465c
JJunney <idwn1203@gmail.com>
2018-12-24 20:13:22 +0900

    copy folder as filelist

--- src/Sol/file.java
+++ src/Sol/file.java
@@ -1,7 +1,7 @@
 package Sol;
 
 import java.io.File;
-import java.util.Formatter;
+import java.io.IOException;
 
 public class file {
 
@@ -30,21 +30,49 @@ public class file {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}*/
-File file =new File("C:\\Users\\소유자\\Desktop\\test");
 
-String name =file.getName();
-String list[] = file.list();
+
+/*
+ * 폴더안에있는 목록대로 폴더 생성
+*/
+String root ="C:\\Users\\소유자\\Desktop\\test";
+String objectroot ="C:\\Users\\소유자\\Desktop\\test\\22";
+File file =new File(root);
+String[] F_list=file.list();
+String temp;
+for(int i=0;i<F_list.length;i++) {
+	System.out.println("name is "+i+F_list[i]);
+
+	File file12 =new File(F_list[i]);
+		file12.mkdir();
+	
+
+
+/*
+ * 폴더확인 있으면 안으로 없으면 리스트 출력 
+*/
+
+
+
+/*String name =file.getName();
+String Fo_list[] = file.list();
+//file의 리스트 출력 
+
 System.out.println("name is "+name);
-for(int i=0;i<list.length;i++) {
-	System.out.println(list[i]);
+for(int i=0;i<Fo_list.length;i++) {
+	System.out.println(Fo_list[i]);
 }
-File file1 = new File("C:\\Users\\소유자\\Desktop\\test\\2");
-File file2 = new File("C:\\\\Users\\\\소유자\\\\Desktop\\\\test\\\\245");
 
-File file3 =new File(list[0]+50);
-file3.mkdir();
+String Fo_mk = destintion+Fo_list
+
+File file1 = new File("test");
+File file2 = new File("C:\\Users\\소유자\\Desktop\\test\\245");
+for(int i=0;i<list.length;i++) {
+File[] filea =new File[i];
+//filea.
+}
+//file 폴더이름변경
 if(!file1.renameTo(file2)) {
-
 	System.out.println("test");
 }
 final Formatter x; 
@@ -57,25 +85,26 @@ catch(Exception e) {
 	System.out.println("error");
 }
 
-	}
 
+*/
 	
 	
 	
 	
 	
 	
+/*	string result 
+	for() {
+		for() {
+			
+		}	
+	}
+	
+	*/
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}
+	}
 	
 }
