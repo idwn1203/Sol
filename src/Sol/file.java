c96360192c6d749df9bcc3083a6500dc68eae84c
JJunney <idwn1203@gmail.com>
2018-12-27 20:33:08 +0900

    make Compare Addrev

--- src/Sol/file.java
+++ src/Sol/file.java
@@ -1,10 +1,14 @@
 package Sol;
 
 import java.io.File;
+import java.io.IOException;
 
 public class file {
 
-	@SuppressWarnings("null")
+	public file(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public static void main(String[] args) {
 
 		
@@ -38,39 +42,49 @@ public class file {
 String root ="C:\\Users\\소유자\\Desktop\\test";
 String objectroot ="C:\\\\Users\\\\소유자\\\\Desktop\\\\test1";
 File file =new File(root);
-String[] F_list=file.list();
-
-
 int T_cnt=0;
 int I_cnt=0;
 int V_cnt=0;
 
+//리스트 저장 
+String[] F_list=file.list();
 String[] T_file= new String[F_list.length];
 String[] I_file= new String[F_list.length];
 String[] V_file= new String[F_list.length];
+
 /*
  * 폴더안에있는 목록출력
 */
 
 for(int i=0;i<F_list.length;i++) {
 	//txt파일일경우
-	if(F_list[i].contains("txt")) {
+	if(F_list[i].endsWith("txt")) {
 		T_file[T_cnt]=F_list[i];
 		T_cnt++;
 	}
 	//사진일경우
-	if(F_list[i].contains("jpg")||F_list[i].contains("png")) {
+	if(F_list[i].endsWith("jpg")||F_list[i].endsWith("png")) {
 		I_file[I_cnt]=F_list[i];
 		I_cnt++;
 		}
-	if(F_list[i].contains("avi")) {
+	if(F_list[i].endsWith("avi")) {
 		V_file[V_cnt]=F_list[i];
 		V_cnt++;
-		}
+	}
 }
 
-System.out.println("파일목록"+"\n"+T_file+"image\n"+I_file+"video\n"+V_file);
-
+System.out.println("txt 파일목록");
+for(int i=0;i<T_cnt;i++) {
+	System.out.println(T_file[i]);
+}
+System.out.println("image파일");
+for(int i=0;i<I_cnt;i++) {
+	System.out.println(I_file[i]);
+}
+System.out.println("video파일");
+for(int i=0;i<V_cnt;i++) {
+	System.out.println(V_file[i]);
+}
 /*
  * 폴더안에있는 목록대로 폴더 생성
 */
@@ -79,6 +93,57 @@ for(int i=0;i<F_list.length;i++) {
 	temp.mkdir();
 	temp.delete();
 }
+
+
+/*
+ * 축약어 추출 프로그램 
+ * */
+
+String Senten= "Graphinc User Interpace";
+String Abbrev= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+
+
+/*
+ * 축약어 추출 프로그램 
+ * */
+
+for(int i=0;i<Senten.length();i++) {
+	for(int j=0;j<Abbrev.length();j++) {
+		if(Senten.charAt(i)==Abbrev.charAt(j))
+		System.out.print(Abbrev.charAt(j));
+	}
+
+
+}
+
+
+/*
+ * 파일생성 
+ * */
+
+File mdfile = new File("C:\\Users\\소유자\\Desktop\\test1\\test.md");
+try {
+	mdfile.createNewFile();
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+
+/*
+ * 특정단어 삽입 축약어 추출 프로그램 
+ * */
+
+
+/*
+ * 파일이름 변경일괄 변경하기
+ */
+
+
+
 
 /*
  * 폴더확인 있으면 안으로 없으면 리스트 출력 
