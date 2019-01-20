ef25d85f0c0264504cc838055b8a1b55a7f61215
JJunney <idwn1203@gmail.com>
2019-01-21 07:12:32 +0900

    update for recordVoice

--- App/app/src/main/java/com/example/iic_common/myapplication/MainActivity.java
+++ App/app/src/main/java/com/example/iic_common/myapplication/MainActivity.java
@@ -5,18 +5,25 @@ import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button Atmega;
     Button Aduino;
-    Button com;
+    Button Start;
+    Button Stop;
+    Button Send;
 
+
+    private MediaRecorder mRecorder = null;
+    int cnt = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +31,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("Idwn");
         // 버튼 선언
-        com=(Button)findViewById(R.id.com);
+        Start = (Button) findViewById(R.id.Start);
+        Stop = (Button) findViewById(R.id.Stop);
+        Send = (Button) findViewById(R.id.Send);
         // 버튼 기능 구현
-        com.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                setTitle("com");
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                cnt =1;
+                StartRec();
+                //StartRec();
+                //CompareVoice();
+                //SendVoice();
             }
         });
+        Stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cnt =2;
+                StopRec();
+            }
+        });
+        Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CompareVoice();
+                if (cnt == 1)
+                    Toast.makeText(getApplicationContext(), "일치", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(), "불일치", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+/*
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.On:
+                break;
+            case R.id.Off:
+                break;
+        }
+    }*/
+
+    private void StartRec() {
+        setTitle("Start");
+        //mRecorder = new MediaRecorder();
+        //mRecorder.start();
+    }
+
+    private void StopRec() {
+        setTitle("Stop");
+        // mRecorder.stop();
+    }
+
+    private void SendVoice() {
+        setTitle("test2");
+    }
+
+    private void CompareVoice() {
+        setTitle("Controller");
     }
 }
+
+
